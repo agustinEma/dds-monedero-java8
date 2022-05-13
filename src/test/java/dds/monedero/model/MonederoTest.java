df21694aa5b4eff4da3ab103733f8a66791e5be8
agustinEma <ayanes@frba.utn.edu.ar>
2022-05-13 03:31:30 -0300

    Update MonederoTest.java
    fix: test

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -4,9 +4,11 @@ import dds.monedero.exceptions.MaximaCantidadDepositosException;
 import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
@@ -19,12 +21,15 @@ public class MonederoTest {
 
   @Test
   void Poner() {
+
     cuenta.poner(1500);
+    assertEquals(1500, cuenta.getSaldo());
   }
 
   @Test
   void PonerMontoNegativo() {
-    assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
+    MontoNegativoException thrown = assertThrows(MontoNegativoException.class, () -> cuenta.poner(-1500));
+    Assertions.assertEquals("-1500.0: el monto a ingresar debe ser un valor positivo", thrown.getMessage());
   }
 
   @Test
@@ -32,24 +37,27 @@ public class MonederoTest {
     cuenta.poner(1500);
     cuenta.poner(456);
     cuenta.poner(1900);
+    assertEquals(3856, cuenta.getSaldo());
   }
 
   @Test
   void MasDeTresDepositos() {
-    assertThrows(MaximaCantidadDepositosException.class, () -> {
+    MaximaCantidadDepositosException thrown = assertThrows(MaximaCantidadDepositosException.class, () -> {
           cuenta.poner(1500);
           cuenta.poner(456);
           cuenta.poner(1900);
           cuenta.poner(245);
     });
+    Assertions.assertEquals("Ya excedio los 3 depositos diarios", thrown.getMessage());
   }
 
   @Test
   void ExtraerMasQueElSaldo() {
-    assertThrows(SaldoMenorException.class, () -> {
+    SaldoMenorException thrown = assertThrows(SaldoMenorException.class, () -> {
           cuenta.setSaldo(90);
           cuenta.sacar(1001);
     });
+    Assertions.assertEquals("No puede sacar mas de " + cuenta.getSaldo() + " $", thrown.getMessage());
   }
 
   @Test
@@ -62,7 +70,9 @@ public class MonederoTest {
 
   @Test
   public void ExtraerMontoNegativo() {
-    assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
+
+    MontoNegativoException thrown = assertThrows(MontoNegativoException.class, () -> cuenta.sacar(-500));
+    Assertions.assertEquals("-500.0: el monto a ingresar debe ser un valor positivo", thrown.getMessage());
   }
 
 }
